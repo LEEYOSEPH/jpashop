d6ad7167d2ad3d3f09d97d1cceedc778a5693e3c
LEEYOSEPH <laksmi0628@gmail.com>
2022-06-27 19:58:04 +0900

    주문 조회 : V5 JPA에서 DTO 직접 조회 - 컬렉션 조회 최적화

--- src/main/java/jpabook/jpashop/api/OrderApiController.java
+++ src/main/java/jpabook/jpashop/api/OrderApiController.java
@@ -70,6 +70,11 @@ public class OrderApiController {
     public List<OrderQueryDto> ordersV4() {
         return orderQueryRepository.findOrderQueryDtos();
     }
+
+    @GetMapping("/api/v5/orders")
+    public List<OrderQueryDto> ordersV5() {
+        return orderQueryRepository.findAllByDto_optimization();
+    }
     @Getter
     static class OrderDto {
 
